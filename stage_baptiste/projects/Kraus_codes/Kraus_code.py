80792d32d4d4eb78e5da72a49dc2c87c1d86be38
Jayboud <jeremie.boudreault@gmail.com>
2023-06-28 12:31:49 -0400

    somefigs

--- stage_baptiste/projects/Kraus_codes/Kraus_code.py
+++ stage_baptiste/projects/Kraus_codes/Kraus_code.py
@@ -12,19 +12,21 @@ from stage_baptiste.homemades.KrausOperator_JV import opListsBs2
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-
+d = 4
+j = 0
 delta = 0.15
 dim = 75
-qubit = GKP(2,0,delta,dim)
-
-a = destroy(dim)
-ad = dag(a)
-n = ad*a
-idCav = qeye(dim)
-Klist = opListsBs2(qubit)  # getting the list of operators
+qubit = GKP(2,j,delta,dim)
 
 
-# define big spin
+# getting useful ops
+a = destroy(dim)  # annihilation operator
+n_op = a.dag()*a  # number operator
+idCav = qeye(dim)  # ideal cavity
+Klist = opListsBs2(qubit)  # error operators
+
+
+# define errors
 Kgg = Klist[0][0]*Klist[1][0]
 Kge = Klist[0][0]*Klist[1][1]
 Keg = Klist[0][1]*Klist[1][0]
@@ -33,12 +35,20 @@ Kee = Klist[0][1]*Klist[1][1]
 xvec = np.linspace(-5,5,200)
 
 
+# ---rotating qubit state in d basis so it matches the qubit basis---
+# qubit_in_d4_state = (GKP(d,0,delta,dim).state+np.exp(1j*1*pi/4)*GKP(d,1,delta,dim).state-GKP(d,2,delta,dim).state+np.exp(1j*1*pi/4)*GKP(d,3,delta,dim).state).unit()
+# rotlist = np.linspace(0,7*pi/4,5)
+# rot = mesolve(n_op, qubit_in_d4_state, rotlist, [], [])
+# psi = rot.states[-1]
+# -------------------------------------------------------------------
+psi = qubit.state
 # psi = basis(Ncav)  # Test by starting from vacuum, always measuring g
-psi = qubit.state  # Test by starting from vacuum, always measuring g
 fig, axes = plt.subplots(3, 3, figsize=(12,12))
 for i in range(9):
     psi = (Kgg*psi).unit()  # Important to normalize the state as this is not a unitary evolution
     Wpsi = wigner(psi, xvec, xvec)
     wlim = abs(Wpsi).max()
     axes[i%3,i//3].contourf(xvec, xvec, Wpsi, 100, norm=mpl.colors.Normalize(-wlim, wlim),cmap=mpl.colormaps['RdBu'])
-plt.savefig("kraus_JV")
+plt.suptitle(r"$|0\rangle_{(2)}$",fontsize='xx-large')
+# plt.suptitle(r"$|0\rangle_{(2)}$ in basis $d=4,m=n=1$",fontsize='xx-large')
+plt.savefig(f"/Users/jeremie/Desktop/Stage_Baptiste/stage_baptiste/projects/Kraus_codes/figs/Kraus_d=2,j=0")
